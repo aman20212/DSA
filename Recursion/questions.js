0c8ec6e48d8e759863dc85b06f02bcb7847597a5
Aman Singh <amansingh@Amans-MacBook-Pro.local>
2026-08-14 06:07:42 +0530

    Updated Recursion Questions

--- Recursion/questions.js
+++ Recursion/questions.js
@@ -14,3 +14,29 @@ function factorial(num) {
     return num * factorial(num - 1);
   }
 }
+
+function productOfArray(arr) {
+  console.log(arr);
+  if (arr.length === 0) {
+    return 1;
+  } else {
+    return arr[0] * productOfArray(arr.slice(1));
+  }
+}
+
+
+function recursiveRange(num){
+   if (num === 0) return 0;
+   else {
+    return num + recursiveRange(num-1);
+   }
+}
+
+function fib(n){
+  // add whatever parameters you deem necessary - good luck! 
+  if (n === 1 || n === 2) return 1;
+  else {
+    return fib(n-1) + fib(n-2);
+  }
+}
+
--- Recursion/questions.py
+++ Recursion/questions.py
@@ -13,3 +13,36 @@ def factorial(num):
     else:
         return num * factorial(num-1)
 print(factorial(4))
+
+
+def productOfArray(arr):
+    if len(arr) == 0:
+        return 1
+    else:
+        return arr[0] * productOfArray(arr[1:])
+print(productOfArray([1,2,3,10]))
+
+def fib(n):
+    # Base cases: the 1st and 2nd Fibonacci numbers are both 1
+    if n == 1 or n == 2:
+        return 1
+    # Recursive case: sum of the previous two numbers
+    return fib(n - 1) + fib(n - 2)
+
+# Examples
+print(fib(1))  # Output: 1
+print(fib(2))  # Output: 1
+print(fib(4))  # Output: 3
+print(fib(6))  # Output: 8
+
+
+def fib_memo(n, memo={}):
+    if n in memo:
+        return memo[n]
+    if n == 1 or n == 2:
+        return 1
+    
+    memo[n] = fib_memo(n - 1, memo) + fib_memo(n - 2, memo)
+    return memo[n]
+
+print(fib_memo(50))  # Output: 12586269025 (Calculates instantly!)
